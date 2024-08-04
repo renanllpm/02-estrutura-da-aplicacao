c56d443b4b764a8d09d9d890df62c0fb02155ef3
Renan <renanllpm@gmail.com>
2024-08-03 22:13:07 -0300

    Validando formulário

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,10 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 import { Avatar } from './Avatar'
 
-export function Comment({content}) {
+export function Comment({ content, onDeleteComment }) {
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/renanllpm.png" />
@@ -15,7 +18,7 @@ export function Comment({content}) {
                 Cerca de 1h atrás
               </time>
             </div>
-            <button title="Deletar comentário">
+            <button title="Deletar comentário" onClick={handleDeleteComment}>
               <Trash size={24} />
             </button>
           </header>
